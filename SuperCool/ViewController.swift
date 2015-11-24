2af41080248eb6ea1a5248f4013fc6e7ff35e170
Peter Rule <peterrule27@gmail.com>
2015-11-24 12:09:36 +0000

    added files for supercool

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -5,11 +5,17 @@
 //  Created by Peter Rule on 20/11/2015.
 //  Copyright © 2015 R And R Develop. All rights reserved.
 //
+// used textcraft.net for Boom App logo
+// used freepik.com for background
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var unCoolButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeMeNotSoUncool(sender: AnyObject) {
+        coolLogo.hidden = false
+        coolBg.hidden = false
+        unCoolButton.hidden = true
+    }
 
 }
 
